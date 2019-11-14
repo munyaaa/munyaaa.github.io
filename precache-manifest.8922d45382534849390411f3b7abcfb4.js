cd72eac9d7962aefe6c5a5030abf2ab644349c05
munyaaa <jessicajimantoro@gmail.com>
2019-11-14 13:38:50 +0700

    Updates

---
+++ precache-manifest.8922d45382534849390411f3b7abcfb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe4a13ac2d798447f5110d63a5001e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acd843bcb34a1b71bfb9",
+    "url": "/static/css/main.55db44ac.chunk.css"
+  },
+  {
+    "revision": "631987f6faeba63049dc",
+    "url": "/static/js/2.0a77693d.chunk.js"
+  },
+  {
+    "revision": "acd843bcb34a1b71bfb9",
+    "url": "/static/js/main.c71e763f.chunk.js"
+  },
+  {
+    "revision": "546c7cb693797206cc26",
+    "url": "/static/js/runtime-main.c202b2d4.js"
+  }
+]);
--- src/pages/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home'
-export { default as Experiences } from './Experiences'
-export { default as Skills } from './Skills'
-export { default as Projects } from './Projects'
